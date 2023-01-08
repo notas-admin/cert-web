e15fb158154d2a05306484707c1753c9b9052a0a
fprodrigues <dombalder@gmail.com>
2023-01-07 23:21:21 -0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
-const webpack = require("webpack");
-const path = require("path");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
-  webpack(config, options) {
-    config.resolve.modules.push(path.resolve("./"));
-    return config;
-  },
-});
+module.exports = nextConfig
